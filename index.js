7e8f5f3931c7c60c54031009aa7e14eecfcb0ce5
XLuoChen <1425145741@qq.com>
2016-07-25 10:37:33 +0800

    static method and instance method

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 'use strict';
 
-class person{
+class person {
   constructor(height, weight) {
     this.height = height;
     this.weight = weight;
   }
 }
 
-const baiying = new person(155,40);
+const baiying = new person(155, 40);
 console.log(`height: ${baiying.height}, weight: ${baiying.weight}`);
 
-class girl extends person{
-  constructor(height, weight,boyfriend) {
+class girl extends person {
+  constructor(height, weight, boyfriend) {
     super(height, weight);
     this.boyfriend = boyfriend;
   }
+
+  static getSex() {
+    return 'girl';
+  }
+
+  printInfo() {
+    console.log(`height: ${this.height},\
+ weight: ${this.weight}, \
+ boyfriend：${this.boyfriend}`);
+  }
 }
 
-const yangzhanmei = new girl(155,50,'tom');
-console.log(`height: ${yangzhanmei.height},\
- weight: ${yangzhanmei.weight}, \
- boyfriend：${yangzhanmei.boyfriend}`);
+const yangzhanmei = new girl(155, 50, 'tom');
+console.log(girl.getSex());
+yangzhanmei.printInfo();
