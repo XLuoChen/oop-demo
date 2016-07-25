347525439cf4c2df92d1b3ff4f1e5053fcc1024b
XLuoChen <1425145741@qq.com>
2016-07-25 10:22:08 +0800

    extends and instant object

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ class person{
 
 const baiying = new person(155,40);
 console.log(`height: ${baiying.height}, weight: ${baiying.weight}`);
+
+class girl extends person{
+  constructor(height, weight,boyfriend) {
+    super(height, weight);
+    this.boyfriend = boyfriend;
+  }
+}
+
+const yangzhanmei = new girl(155,50,'tom');
+console.log(`height: ${yangzhanmei.height},\
+ weight: ${yangzhanmei.weight}, \
+ boyfriend：${yangzhanmei.boyfriend}`);
